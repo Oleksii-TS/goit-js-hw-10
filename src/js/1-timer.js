5c839b5de9697b3887d1ec46cd41ece2eb0da58f
Oleksii <o.s.tymoshchuk@gmail.com>
2025-05-09 15:25:06 +0300

    finish timer

---
+++ src/js/1-timer.js
@@ -0,0 +1,103 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  input: document.getElementById('datetime-picker'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+let userSelectedDate = null;
+let timerId = null;
+
+// Початковий стан
+setButtonState(false);
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    if (selectedDate <= new Date()) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+      });
+      setButtonState(false);
+      return;
+    }
+    userSelectedDate = selectedDate;
+    setButtonState(true);
+  },
+};
+
+flatpickr(refs.input, options);
+
+refs.startBtn.addEventListener('click', () => {
+  if (!userSelectedDate) return;
+
+  setButtonState(false);
+  refs.input.disabled = true;
+  refs.input.classList.add('disabled');
+
+  timerId = setInterval(() => {
+    const now = new Date();
+    const diff = userSelectedDate - now;
+
+    if (diff <= 0) {
+      clearInterval(timerId);
+      updateTimer({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+      refs.input.disabled = false;
+      refs.input.classList.remove('disabled');
+      return;
+    }
+
+    const time = convertMs(diff);
+    updateTimer(time);
+  }, 1000);
+});
+
+function setButtonState(isActive) {
+  refs.startBtn.disabled = !isActive;
+  refs.startBtn.classList.toggle('active-btn', isActive);
+  refs.startBtn.classList.toggle('disabled-btn', !isActive);
+}
+
+function updateTimer({ days, hours, minutes, seconds }) {
+  refs.days.textContent = addLeadingZero(days);
+  refs.hours.textContent = addLeadingZero(hours);
+  refs.minutes.textContent = addLeadingZero(minutes);
+  refs.seconds.textContent = addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
